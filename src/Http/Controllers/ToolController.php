a1b41f595510544c312b407b75e99aa131aeaa4f
spgwebdev <administrator@seniorprogramming.ro>
2020-08-05 13:12:45 +0300

    Update ToolController.php

--- src/Http/Controllers/ToolController.php
+++ src/Http/Controllers/ToolController.php
@@ -20,6 +20,7 @@ class ToolController extends Controller
 
           $field['name'] = ucfirst(__("validation.attributes." . $field['attribute']));
           $field['indexName'] = ucfirst(__("validation.attributes." . $field['attribute']));
+          $field['validationKey'] = $field['validationKey'] ?? $field['attribute'];
 
           $fields[] = $field;
         }
@@ -33,6 +34,11 @@ class ToolController extends Controller
     public function store()
     {
         $validations = config('nova-profile-tool.validations');
+        
+        // unique email except auth user's email
+        if($validations['email']){
+            $validations['email'] = $validations['email'].'|unique:users,email,'.auth()->id();
+        }
 
         request()->validate($validations);
 
